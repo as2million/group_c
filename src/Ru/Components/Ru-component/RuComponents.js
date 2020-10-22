8f09051f54b92568e5b78e724b18e9709ffe8275
Ruru <k718198@gmail.com>
2020-10-22 20:57:34 +0800

    20201022 08:57p.m. 製作產品列表樣式

--- src/Ru/Components/Ru-component/RuComponents.js
+++ src/Ru/Components/Ru-component/RuComponents.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "Ru/Components/Ru-component/Style.scss";
-import RuShowWidth from "Ru/Components/Ru-ShowWidth/RuShowWidth";
 import { Container, Col, Row } from "react-bootstrap";
+import RuShowWidth from "Ru/Components/Ru-ShowWidth/RuShowWidth";
+import star from "Ru/Components/Ru-component/Images/star.svg";
+import cart from "Ru/Components/Ru-component/Images/cart.svg";
 function RuComponents() {
   return (
     <>
+      <div style={{ height: "135px", backgroundColor: "#FF5151" }}>
+        我是navbar
+      </div>
       {/* 如果元件有定義key或ref, 它們並不會向下傳資料, 它們不屬於props */}
       <RuShowWidth />
       <div className="ru-mainImg">
-        <p>享受美食 不需要理由</p>
+        <h1>享受美食 不需要理由</h1>
       </div>
       <div className="ru-containerA">
         <div className="ru-optionWarp">
@@ -24,59 +29,73 @@ function RuComponents() {
             <button className="ru-button ru-button4">蔬菜箱</button>
           </section>
         </div>
-        <Container className="ru-noMargin ru-containerB">
-          <Row className="ru-noPadding ru-row">
-            <Col className="ru-noPadding ru-col ru-col1">
-              <div className="ru-itemWarp ru-itemWarp1">
-                <div className="ru-item ru-item1">這裡是商品1</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col2">
-              <div className="ru-itemWarp ru-itemWarp2">
-                <div className="ru-item ru-item2">這裡是商品2</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col3">
-              <div className="ru-itemWarp ru-itemWarp3">
-                <div className="ru-item ru-item3">這裡是商品3</div>
-              </div>
-            </Col>
-          </Row>
-          <Row className="ru-noPadding ru-row">
-            <Col className="ru-noPadding ru-col ru-col1">
-              <div className="ru-itemWarp ru-itemWarp1">
-                <div className="ru-item ru-item1">這裡是商品1</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col2">
-              <div className="ru-itemWarp ru-itemWarp2">
-                <div className="ru-item ru-item2">這裡是商品2</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col3">
-              <div className="ru-itemWarp ru-itemWarp3">
-                <div className="ru-item ru-item3">這裡是商品3</div>
-              </div>
-            </Col>
-          </Row>
-          <Row className="ru-noPadding ru-row">
-            <Col className="ru-noPadding ru-col ru-col1">
-              <div className="ru-itemWarp ru-itemWarp1">
-                <div className="ru-item ru-item1">這裡是商品1</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col2">
-              <div className="ru-itemWarp ru-itemWarp2">
-                <div className="ru-item ru-item2">這裡是商品2</div>
-              </div>
-            </Col>
-            <Col className="ru-noPadding ru-col ru-col3">
-              <div className="ru-itemWarp ru-itemWarp3">
-                <div className="ru-item ru-item3">這裡是商品3</div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+
+        <div className="itemWarp itemWarp1">
+          <div className="ru-item ru-item1">
+            <section className="item-img"></section>
+            <section className="item-info-warp">
+              <h2>慢煮嫩雞胸-蒜味香草</h2>
+              <section className="star-warp">
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <p className="num commentNum">1180則評論</p>
+                <p className="num buyNum">234已購買</p>
+                <h3>$130</h3>
+              </section>
+              <button>
+                <img className="cart" src={cart} />
+              </button>
+            </section>
+            <section className="item-button-warp"></section>
+          </div>
+          <div className="ru-item ru-item1">
+            <section className="item-img"></section>
+            <section className="item-info-warp">
+              <h2>慢煮嫩雞胸-蒜味香草</h2>
+              <section className="star-warp">
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <p className="num commentNum">1180則評論</p>
+                <p className="num buyNum">234已購買</p>
+                <h3>$130</h3>
+              </section>
+              <button>
+                <img className="cart" src={cart} />
+              </button>
+            </section>
+            <section className="item-button-warp"></section>
+          </div>
+          <div className="ru-item ru-item1">
+            <section className="item-img"></section>
+            <section className="item-info-warp">
+              <h2>慢煮嫩雞胸-蒜味香草</h2>
+              <section className="star-warp">
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <img className="star" src={star} />
+                <p className="num commentNum">1180則評論</p>
+                <p className="num buyNum">234已購買</p>
+                <h3>$130</h3>
+              </section>
+              <button>
+                <img className="cart" src={cart} />
+              </button>
+            </section>
+            <section className="item-button-warp"></section>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ height: "222px", backgroundColor: "#FF5151" }}>
+        我是footer
       </div>
     </>
   );
