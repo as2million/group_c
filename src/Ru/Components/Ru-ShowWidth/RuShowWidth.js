2a023db4461d5485e34b80fafaf0f6a6ec748ff9
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2020-10-26 23:26:15 +0800

    20201026 11:26p.m. 商品列表切版

--- src/Ru/Components/Ru-ShowWidth/RuShowWidth.js
+++ src/Ru/Components/Ru-ShowWidth/RuShowWidth.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import "Ru/Components/Ru-ShowWidth/Style.scss";
 
 // 監控瀏覽器寬度
 function ShowWidth(props) {
   useEffect(() => {
     // const $showWidth = document.querySelector("#showWidth");
     window.addEventListener("resize", (e) => {
-      console.log(e.target.innerWidth);
+      // console.log(e.target.innerWidth);
       SetWidth(e.target.innerWidth);
     });
     console.log("Hi");
--- src/Share/Components/Card/Card.js
+++ src/Share/Components/Card/Card.js
@@ -8,17 +8,18 @@ function Card(props) {
   // comment 有幾則評論
   // buy 有多少人買
   // price 價格
-  // id 不同元件id
-  // parentId 不同元件父母id
-  const { title, comment, buy, price, id, parentId } = props;
+  // id 不同元件id => addCart-btn-n n為自訂數
+  // parentId 不同元件父母id => addCart-btn-warp-n n為自訂數
+  // imgId 產品圖片 => card-img-n n為1~9
+  const { title, comment, buy, price, id, parentId, imgId } = props;
 
   return (
     <>
-      <div className="card">
+      <div className="card-container">
         {/* item圖片s */}
         <section className="card-img-warp">
           <a href="" className="card-link">
-            <img className="card-img card-img-n"></img>
+            <img className="card-img" id={imgId}></img>
           </a>
           <div className="card-abs">
             <AddFavorite />
@@ -48,7 +49,7 @@ function Card(props) {
         {/* item資訊e */}
         {/* 加入購物車按鈕s */}
         <section className="card-addCartWarp">
-          <AddCart target={"target"} id={id} parentId={parentId}/>
+          <AddCart target={"target"} id={id} parentId={parentId} />
         </section>
         {/* 加入購物車按鈕e */}
       </div>
