c1013538d39d33dfc652df1f7d6242fbe20a4693
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-26 23:16:37 +0800

    use svg as a component

--- src/Share/Components/Input/SelectBox.js
+++ src/Share/Components/Input/SelectBox.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./Input.scss";
-import triangleArrow from "./../../image/triangleArrow.svg"
+import { ReactComponent as TriangleArrow } from "./../../image/triangleArrow.svg";
 
 function SelectBox() {
   return (
     <>
-      <div class="iris-select-wrapper" >
+      <div class="iris-select-wrapper">
         <select class="form-control iris-select" id="exampleFormControlSelect1">
           <option>1</option>
           <option>2</option>
           <option>3</option>
         </select>
-        <div class="iris-whitebox"></div>
-        <img src={triangleArrow} alt="arrow" class="iris-triangle"/>
+        <div
+          class="iris-whitebox"
+        ></div>
+        <TriangleArrow className="iris-triangle" />
       </div>
     </>
   );
