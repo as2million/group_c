5114046b63553ab7791f8dc2496550a113e1d070
Jess <jssanji03@gmail.com>
2020-10-26 18:04:10 +0800

    navBar finiahed

--- src/Share/Components/NavBar/NavBar6.js
+++ src/Share/Components/NavBar/NavBar6.js
@@ -16,85 +16,90 @@ import { ReactComponent as ShoppingAmount } from '../../Images/SVG/navbar-cartNu
 function MyNavbar(props) {
   const [count, setCount] = useState(0)
   const [shoppingList, setShoppingList] = useState('0')
+
+  function myFunction() {
+    const x = document.getElementById('myNavBar')
+    if (x.className === 'nav') {
+      x.className += ' responsive'
+    } else {
+      x.className = 'nav'
+    }
+  }
   return (
     <>
-      <div className="nav">
-        <div className="container">
-          <div className="icons-list nav_toggle">
-            <MenuOutlined />
-          </div>
-
-          <div className="nav_collapse ">
-            <ul className="nav_navigation">
-              <li>
+      <div className="nav" id="myNavBar">
+        <div className="navBar-jess-container">
+          <div className="navBar-jess-navCollapse ">
+            <ul className="navBar-jess-navigation">
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">作伙揪團</a>
               </li>
-              <li className="nav_navigation_item">
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">哈囉小農</a>
               </li>
-              <li className="nav_navigation_item">
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">
-                  尋找美味 <BackArrow />{' '}
+                  尋找美味 <BackArrow className="backArrow" />{' '}
                 </a>
-                <ul className="dropdown">
-                  <li className="dropdown_item">
+                <ul className="navBar-jess-dropdown">
+                  <div className="navBar-jess-triangle"></div>
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">低GI便當</a>
-                    <Monster className="jess-monster1" />
+                    {/* <Monster className="navBar-jess-monster1" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">美味沙拉</a>
-                    <Monster className="jess-monster2" />
+                    {/* <Monster className="navBar-jess-monster2" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">蔬菜箱</a>
-                    <Monster className="jess-monster3" />
+                    {/* <Monster className="navBar-jess-monster3" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">客製化便當</a>
-                    <Monster className="jess-monster4" />
+                    {/* <Monster className="navBar-jess-monster4" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">外送服務</a>
-                    <Monster className="jess-monster5" />
+                    {/* <Monster className="navBar-jess-monster5" /> */}
                   </li>
-                  <div className="triangle"></div>
                 </ul>
               </li>
-              <a href="#home" className="nav_brand">
+              <a href="#home" className="navBar-jess-nav_brand">
                 <Logo />
               </a>
-              <li>
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">專屬優惠</a>
               </li>
-              <li className="nav_navigation_item">
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">關於我們</a>
               </li>
-              <li className="nav_navigation_item">
+              <li className="navBar-jess-navigation_item">
                 <a href="#home">
-                  會員中心 <BackArrow />{' '}
+                  會員中心 <BackArrow className="backArrow" />{' '}
                 </a>
-                <ul className="dropdown">
-                  <li className="dropdown_item">
+                <ul className="navBar-jess-dropdown2">
+                  <div className="navBar-jess-triangle2"></div>
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">訂單管理</a>
-                    <Monster className="jess-monster1" />
+                    {/* <Monster className="navBar-jess-monster1" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">修改會員資料</a>
-                    <Monster className="jess-monster2" />
+                    {/* <Monster className="navBar-jess-monster2" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">我的最愛</a>
-                    <Monster className="jess-monster3" />
+                    {/* <Monster className="navBar-jess-monster3" /> */}
                   </li>
-                  <li className="dropdown_item">
+                  <li className="navBar-jess-dropdown_item">
+                    <a href="#home">我的怪獸</a>
+                    <Monster className="navBar-jess-monster4" />
+                  </li>
+                  <li className="navBar-jess-dropdown_item">
                     <a href="#home">登出</a>
-                    <Monster className="jess-monster4" />
+                    {/* <Monster className="navBar-jess-monster5" /> */}
                   </li>
-                  <li className="dropdown_item">
-                    <a href="#home">外送服務</a>
-                    <Monster className="jess-monster5" />
-                  </li>
-                  <div className="triangle"></div>
                 </ul>
               </li>
 
@@ -104,14 +109,18 @@ function MyNavbar(props) {
                   content={shoppingList}
                   title="我的購買清單"
                   trigger="hover"
+                  className="navbar-jess-popover"
                 >
-                  <ShoppingCart className="dropdown_item" />
-                  <ShoppingAmount className="jess-navbarAmount" />
+                  <ShoppingCart className="navbar-jess-ShopingCart" />
+                  <ShoppingAmount className="jess-navbarCartAmount" />
                   <span className="jess-navbarCartNum">{count}</span>
                 </Popover>
               </li>
             </ul>
           </div>
+        </div>
+        <div className="icons-list nav_toggle" onClick={myFunction}>
+          <MenuOutlined />
         </div>
       </div>
     </>
