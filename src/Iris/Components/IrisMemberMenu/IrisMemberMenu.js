a8c62933c8fd167b6b478b5f43ba5e8c01f109a4
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-27 00:05:35 +0800

    make member menu

--- src/Iris/Components/IrisMemberMenu/IrisMemberMenu.js
+++ src/Iris/Components/IrisMemberMenu/IrisMemberMenu.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./IrisMemberMenu.scss";
+import { ReactComponent as UserProfileIcon } from "./Images/user_profile.svg";
+import { ReactComponent as BeastieIcon } from "./Images/beastie_icon.svg";
+import { ReactComponent as OrderIcon } from "./Images/order_icon.svg";
+import { ReactComponent as PlateIcon } from "./Images/plate_icon.svg";
+import { ReactComponent as GroupOrderIcon } from "./Images/group_order.svg";
+import { ReactComponent as MyFavIcon } from "./Images/my_fav.svg";
+import { ReactComponent as MyCommentIcon } from "./Images/my_comment.svg";
 
 function IrisMemberMenu() {
   return (
@@ -28,20 +35,36 @@ function IrisMemberMenu() {
                 <div>怪獸幣</div>
                 <div>480</div>
               </div>
-              {/* ---------- menu ----------- */}
-              <div class="iris-menu-item">1234</div>
+            </div>
+            {/* ---------- menu ----------- */}
+            <div class="iris-mune-item-wrapper d-flex d-flex flex-wrap">
+              <div class="iris-menu-item">
+                <UserProfileIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <BeastieIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <OrderIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <PlateIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <GroupOrderIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <MyFavIcon class="iris-menu-icon" />
+              </div>
               <div class="iris-menu-hr"></div>
-              <div class="iris-menu-item">1234</div>
+              <div class="iris-menu-item">
+                <MyCommentIcon class="iris-menu-icon" />
+              </div>
             </div>
           </div>
         </div>
