4a5febb81aababce62900c35a0d5dc45ee57a585
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-27 17:01:48 +0800

    20201027 05:01pm update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import IrisApp from "./Iris/IrisApp";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <IrisApp />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
+// import App from "./App";
+import IrisAppTest from "./IrisAppTest";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IrisAppTest />
   </React.StrictMode>,
   document.getElementById("root")
 );
