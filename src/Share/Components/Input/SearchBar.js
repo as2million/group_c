9e5b6a903ea72fbe77e5a3a04c291c8b3dddf12e
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-27 13:15:50 +0800

    update searchbar

--- src/Share/Components/Input/SearchBar.js
+++ src/Share/Components/Input/SearchBar.js
@@ -4,25 +4,42 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./Input.scss";
 import "./SearchBar.scss";
-import searchIcon from "./../../image/search_icon.svg";
+import { ReactComponent as SearchIcon } from "./../../image/search_icon.svg";
 
 function SearchBar() {
-  // function lightenBorder(e){
-  //   document.querySelector("#search").style['background-color'] = '#f00';
-  // }
+  function lightenBorder() {
+    document.querySelector(".iris-input-field").style["box-shadow"] =
+      "0 0 0.5rem #f5a016";
+    document.querySelector(".iris-input-field").style["border-color"] = "#fff";
+    document.querySelector(".iris-img").style.fill = "#F5CB87";
+  }
+  window.onclick = (e) => {
+    if (e.target.id !== "iris-search") {
+      document.querySelector(".iris-input-field").style["box-shadow"] = "none";
+      document.querySelector(".iris-input-field").style["border-color"] =
+        "#858585";
+      document.querySelector(".iris-img").style.fill = "#c2c4ca";
+    }
 
+    // console.log(typeof(e.target))
+    // document.querySelector(".iris-input-field").style["box-shadow"] = "0 0 0.5rem #faf";
+    // document.querySelector(".iris-input-field").style["border-color"] = "#fff";
+  };
   return (
     <>
       <div class="iris-input-field first-wrap">
         <div class="svg-wrapper">
-          <img class="iris-img" src={searchIcon} />
+          {/* <img class="iris-img" src={searchIcon} /> */}
+          <SearchIcon class="iris-img"/>
         </div>
         <input
           class="iris-searchbar"
-          id="search"
+          id="iris-search"
           type="text"
           placeholder="search"
-          // onClick={()=>{lightenBorder()}}
+          onClick={() => {
+            lightenBorder();
+          }}
         />
       </div>
       {/* </div> */}
