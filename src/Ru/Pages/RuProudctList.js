71b8af39a8e9906ab5a59ad0f53edc7b4178d078
Ruru <k718198@gmail.com>
2020-10-27 20:07:13 +0800

    20201027 08:07 ru分支合併

---
+++ src/Ru/Pages/RuProudctList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RuComponent from "Ru/Components/Ru-component/RuComponents";
+
+function RuProudctList() {
+  return (
+    <>
+      <RuComponent />
+    </>
+  );
+}
+
+export default RuProudctList;
