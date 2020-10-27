ad817c0cd30885aff54c4c7252fda148c102480b
chaoting <02115214@gm.scu.edu.tw>
2020-10-28 01:22:18 +0800

    20201028 01:26p.m 呼朋引伴

---
+++ .history/src/Cha/Pages/Cha-Group-Order/ChaGroupOrder_20201027233221.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+import ChaGroupProgressBar from 'Cha/Components/Cha-Group-ProgressBar/ChaGroupProgressBar'
+import ChaGroupOrderCreate from 'Cha/Components/Cha-Group-Order-Create/ChaGroupOrderCreate'
+
+import 'Cha/Pages/Cha-Group-Order/ChaGroupOrder.scss'
+
+function ChaGroupOrder(props) {
+  return (
+    <>
+      <ChaGroupProgressBar />
+
+      <div className="cha-group-wrap">
+        <ChaGroupOrderCreate />
+        
+        </div>
+      </div>
+    </>
+  )
+}
+export default ChaGroupOrder
