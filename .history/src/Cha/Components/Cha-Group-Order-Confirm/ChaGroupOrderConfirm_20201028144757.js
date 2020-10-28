cb1aae4e24195bf866e6d25f6a70bd1b8698cd83
chaoting <02115214@gm.scu.edu.tw>
2020-10-28 22:18:04 +0800

    20201028 10:30p.m 完成揪團各頁面

---
+++ .history/src/Cha/Components/Cha-Group-Order-Confirm/ChaGroupOrderConfirm_20201028144757.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+import './ChaGroupOrderConfirm.scss';
+
+function ChaGroupOrderConfirm(props) {
+  return (
+    <>
+      <div className="cha-bc-lunch-box">
+        <div className="cha-group-confirm-card">
+          <div className="cha-group-confirm-header-div">
+            <div className="cha-group-confirm-logo"></div>
+          </div>
+          <div className="cha-horizontal-line"></div>
+
+          {/* 手機號碼 */}
+          <div className="cha-group-confirm-text-div">
+          <div className="cha-group-confirm-text-div"></div>
+
+            加入揪團成功，如要購買，請點選「加入餐點」紐進行購買
+          </div>
+
+          {/* 提交按鈕 */}
+          <div className="cha-group-confirm-btn-div">
+            <input
+              type="button"
+              value="確認"
+              className="cha-group-confirm-btn"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ChaGroupOrderConfirm;
---
+++ .history/src/Cha/Components/Cha-Group-Order-Menu/ChaGroupOrderMenu_20201028151352.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+import './ChaGroupOrderMenu.scss';
+
+function ChaGroupOrderMenu(props) {
+  return (
+    <>
+<div className="cha-group-menu-fake-"></div>
+    </>
+  );
+}
+export default ChaGroupOrderMenu;
