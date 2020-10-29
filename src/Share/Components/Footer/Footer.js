04d8edb49e61c45b1ec6d62dbd5cddef2e5d7350
Claudia <sydeng102@gmail.com>
2020-10-29 12:11:10 +0800

    20201029 12:10p.m. update footer

--- src/Share/Components/Footer/Footer.js
+++ src/Share/Components/Footer/Footer.js
@@ -6,15 +6,15 @@ import Instagram from '../Images/SVG/instagram.svg';
 function Footer() {
     return (
         <>
-            <div className="navbar-background">
-                <div className="navbar-card">
-                    <div className="navbar-footer-text">
+            <div className="footer-background">
+                <div className="footer-card">
+                    <div className="footer-text">
                         <a href="#"><p>常見問題</p></a>
                         <a href="#"><p>服務條款</p></a>
                         <a href="#"><p>關於我們</p></a>
                         <a href="#"><p>隱私權政策</p></a>
                     </div>
-                    <div className="navbar-icons">
+                    <div className="footer-icons">
                         <a>
                             <img src={Facebook} />
                         </a>
@@ -22,7 +22,7 @@ function Footer() {
                             <img src={Instagram} />
                         </a>
                     </div>
-                    <div className="navbar-copyright">
+                    <div className="footer-copyright">
                         <p>Copyright © 2020 拾餐便當. All rights reserved.</p>
                     </div>
                 </div>
