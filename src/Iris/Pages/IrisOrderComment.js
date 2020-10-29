8f9dab7d320a1e3b2038cd964e94f1ddef66c89e
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-29 14:31:51 +0800

    20201029 02:31pm make orderComment

--- src/Iris/Pages/IrisOrderComment.js
+++ src/Iris/Pages/IrisOrderComment.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import IrisMemberMenu from '../Components/IrisMemberMenu/IrisMemberMenu';
-import IrisDataEdit from '../Components/IrisDataEdit/IrisDataEdit';
-import './IrisProfilePage.scss';
+import IrisUserComment from '../Components/IrisUserComment/IrisUserComment';
+import './IrisMemberPage.scss';
 
 function IrisOrderComment() {
   return (
     <>
-      <div className="container iris-userprofile-container">
+      <div className="container iris-memberpage-container">
         <IrisMemberMenu />
-        <IrisDataEdit />
+        <IrisUserComment />
       </div>
     </>
   );
--- src/Iris/Pages/IrisProfilePage.js
+++ src/Iris/Pages/IrisProfilePage.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import IrisMemberMenu from '../Components/IrisMemberMenu/IrisMemberMenu';
 import IrisDataEdit from '../Components/IrisDataEdit/IrisDataEdit';
-import './IrisProfilePage.scss';
+import './IrisMemberPage.scss';
 
 function IrisProfilePage() {
   return (
     <>
-      <div className="container iris-userprofile-container">
+      <div className="container iris-memberpage-container">
         <IrisMemberMenu />
         <IrisDataEdit />
       </div>
