143d4dbc35a4c13172d94dc882afe23150c43a3c
Jess <jssanji03@gmail.com>
2020-10-29 20:32:05 +0800

    20201029 08:30p.m 商品詳頁-便當-差最後下方

--- src/Jess/Components/JessListD/JessListD.js
+++ src/Jess/Components/JessListD/JessListD.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './JessListD.scss';
+import Button from '../../../Share/Components/Button/Button';
 
 function JessListD() {
   return (
@@ -7,28 +8,68 @@ function JessListD() {
       <div className="container-fluid">
         <div className="container mt-5 jess-proD-bg">
           <h2 className="jess-text-30orange text-center">商品評論</h2>
-          <div className="row">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th colspan="2">First</th>
-                  <th scope="col text-right">Last</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr rowspan="2">
-                  <td>安安 我是</td>
-                  <td>the Bird</td>
-                </tr>
-                <tr>
-                  <td colspan="2">Jacob</td>
-                </tr>
-                <tr>
-                  <td colspan="2">Larry</td>
-                  <td>the Bird</td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="row d-flex jess-msg-tittle justify-content-between">
+            <div className="col-9 d-flex align-items-center">
+              <ul>
+                <li class="jess-text-20orange">整體滿意度</li>
+                <li class="jess-text-15Gray">來自 180 則評分，滿分5顆星</li>
+              </ul>
+            </div>
+            <div className="col-2 jess-msg-todo d-flex align-items-center">
+              <Button className="button-btn" text="我要評論"></Button>
+            </div>
+          </div>
+          <div className="row d-flex jess-msg-content justify-content-between">
+            <div className="col-9 d-flex align-items-center">
+              <ul>
+                <li class="jess-text-15Gray">白*華</li>
+                <li class="jess-text-15brown">
+                  方便又美味 上班吃什麼都不煩惱了
+                </li>
+              </ul>
+            </div>
+            <div className="col-2 jess-msg-todo d-flex align-items-center">
+              <div className="jess-text-15Gray">2020/09/26</div>
+            </div>
+          </div>
+          <div className="row d-flex jess-msg-tittle justify-content-between ">
+            <div className="col-9 d-flex align-items-center">
+              <ul>
+                <li class="jess-text-15Gray">吳*婷</li>
+                <li class="jess-text-15brown">
+                  方便又美味 上班吃什麼都不煩惱了
+                </li>
+              </ul>
+            </div>
+            <div className="col-2 jess-msg-todo d-flex align-items-center">
+              <div className="jess-text-15Gray">2020/09/26</div>
+            </div>
+          </div>
+          <div className="row d-flex jess-msg-content justify-content-between">
+            <div className="col-9 d-flex align-items-center">
+              <ul>
+                <li class="jess-text-15Gray">白*華</li>
+                <li class="jess-text-15brown">
+                  方便又美味 上班吃什麼都不煩惱了
+                </li>
+              </ul>
+            </div>
+            <div className="col-2 jess-msg-todo d-flex align-items-center">
+              <div className="jess-text-15Gray">2020/09/26</div>
+            </div>
+          </div>
+          <div className="row d-flex jess-msg-tittle justify-content-between ">
+            <div className="col-9 d-flex align-items-center">
+              <ul>
+                <li class="jess-text-15Gray">吳*婷</li>
+                <li class="jess-text-15brown">
+                  方便又美味 上班吃什麼都不煩惱了
+                </li>
+              </ul>
+            </div>
+            <div className="col-2 jess-msg-todo d-flex align-items-center">
+              <div className="jess-text-15Gray">2020/09/26</div>
+            </div>
           </div>
         </div>
       </div>
--- src/JessAppTest.js
+++ src/JessAppTest.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import NavBar from './Share/Components/NavBar/NavBar';
+import VNavbar from './Share/Components/VNavbar/VNavbar';
 import JessBento from './Jess/Pages/JessBento';
-import VNavbar from './Share/Components/VNavbar/VNavbar.js';
+import Footer from './Share/Components/Footer/Footer';
 
 function JessAppTest() {
   return (
@@ -11,7 +12,7 @@ function JessAppTest() {
         <NavBar />
         <VNavbar />
         <JessBento />
-        {/* <Footer /> */}
+        <Footer />
       </>
     </Router>
   );
