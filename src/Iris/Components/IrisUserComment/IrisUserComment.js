bedb5f00484b563332d355e073927e05d4189001
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-29 16:27:27 +0800

    20201029 04:27pm update user comment page

--- src/Iris/Components/IrisUserComment/IrisUserComment.js
+++ src/Iris/Components/IrisUserComment/IrisUserComment.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './IrisUserComment.scss';
 import { ReactComponent as WaveLine } from './Images/wave_line.svg';
+import { ReactComponent as IrisMemberLine } from './Images/iris_member_line.svg';
 
 function IrisUserComment() {
   return (
@@ -8,12 +9,14 @@ function IrisUserComment() {
       <div className="container col-9">
         <div className="row justify-content-center iris-content-title-container ">
           <h2 className="iris-comment-title">我的投稿</h2>
-
           <WaveLine />
         </div>
-        <h6 className="iris-comment-note">
-          您共有 <span className="iris-comment-quantity">6</span> 則投稿
-        </h6>
+        <div class="iris-comment-list-container">
+          <h6 className="iris-comment-note">
+            您共有 <span className="iris-comment-quantity">6</span> 則投稿
+          </h6>
+          <IrisMemberLine />
+        </div>
       </div>
     </>
   );
