f0756b21969edb5b7c273f412f8c8959e179af92
tseng-yc <janice8819@gmail.com>
2020-10-30 02:50:34 +0800

    20201030 02:49

--- src/Janice/Components/JanIndex/JanIndex.js
+++ src/Janice/Components/JanIndex/JanIndex.js
@@ -9,29 +9,28 @@ import recommend2 from './Images/Svg/recommend2.svg';
 import map1 from './Images/Svg/map1.svg';
 import map2 from './Images/Svg/map2.svg';
 import stepArrow from './Images/Svg/stepArrow.svg';
+import Button from '../../../Share/Components/Button/Button';
+import star from './Images/Svg/star.svg';
+import starO from './Images/Svg/star-o.svg';
+import Card from '../../../Share/Components/Card/Card';
 
 function JanIndex(props) {
   return (
     <>
-      <div className="box d-flex justify-content-center align-items-center">
+      {/* 線上客服 */}
+      <div className="jan-chat position-absolute d-flex justify-content-center align-items-center position-fixed">
+        <div className="jan-chat-icon"></div>
+      </div>
+      <div className="jan-fake-nav d-flex justify-content-center align-items-center">
         navbar的高度
       </div>
       {/* 輪播牆 */}
       <div className="container-fluid jan-p0">
         <div className="row">
           <div className="jan-index-carousel position-relative">
-            {/* 切換鈕 */}
-            <ul class="position-absolute jan-carousel-dots d-flex justify-content-center">
-              <li class="jan-carousel-dot1"></li>
-              <li class="jan-carousel-dot2"></li>
-              <li class="jan-carousel-dot3"></li>
-              <li class="jan-carousel-dot4"></li>
-              <li class="jan-carousel-dot5"></li>
-            </ul>
-
             {/* 應用程式下載 */}
             {/* 橘底 */}
-            <div className="jan-download-area d-flex justify-content-center">
+            <div className="position-absolute jan-download-area d-flex justify-content-center">
               {/* AppLogo＋應用程式icon */}
               <div className="jan-app-logo"></div>
               <div className="d-flex flex-column align-items-center jan-app-info">
@@ -42,6 +41,15 @@ function JanIndex(props) {
                 </div>
               </div>
             </div>
+
+            {/* 切換鈕 */}
+            <ul class="position-absolute jan-carousel-dots d-flex justify-content-center">
+              <li class="jan-carousel-dot1"></li>
+              <li class="jan-carousel-dot2"></li>
+              <li class="jan-carousel-dot3"></li>
+              <li class="jan-carousel-dot4"></li>
+              <li class="jan-carousel-dot5"></li>
+            </ul>
           </div>
         </div>
       </div>
@@ -49,33 +57,39 @@ function JanIndex(props) {
       {/* 品牌特色 */}
       <div className="container jan-p0">
         <div className="row jan-index-feature d-flex align-items-center justify-content-center">
+          {/* 標題 */}
           <div className="d-flex align-items-center">
             <img alt="" src={titleLeft} />
             <p className="jan-feature-title">品牌特色</p>
             <img alt="" src={titleRight} />
           </div>
-
+          {/* circles */}
           <div className="d-flex jan-circles justify-content-around align-items-center">
+            {/* circle1 */}
             <div className="jan-index-feature-circle d-flex flex-wrap justify-content-center align-items-center">
               <div className="jan-farmer-icon"></div>
               <p className="jan-circle-title">在地小農</p>
             </div>
             <div className="jan-index-feature-smcircle"></div>
+            {/* circle2 */}
             <div className="jan-index-feature-circle d-flex flex-wrap justify-content-center align-items-center">
               <div className="jan-diet-icon"></div>
               <p className="jan-circle-title">產銷履歷</p>
             </div>
             <div className="jan-index-feature-smcircle"></div>
+            {/* circle3 */}
             <div className="jan-index-feature-circle d-flex flex-wrap justify-content-center align-items-center">
               <div className="jan-choices-icon"></div>
               <p className="jan-circle-title">客製化便當</p>
             </div>
             <div className="jan-index-feature-smcircle"></div>
+            {/* circle4 */}
             <div className="jan-index-feature-circle d-flex flex-wrap justify-content-center align-items-center">
               <div className="jan-network-icon"></div>
               <p className="jan-circle-title">揪團訂購</p>
             </div>
             <div className="jan-index-feature-smcircle"></div>
+            {/* circle5 */}
             <div className="jan-index-feature-circle d-flex flex-wrap justify-content-center align-items-center">
               <div className="jan-kcal-icon"></div>
               <p className="jan-circle-title">營養標示</p>
@@ -92,34 +106,166 @@ function JanIndex(props) {
       </div>
 
       {/* 今日推薦 */}
-      <div className="container-fluid jan-p0 jan-recommend">
-        <div className="container mt-5 flex-column jan-p0">
-          {/* 今日推薦 */}
-          <div className="row col-6">
-            <img alt="" src={recommend1} />
+      <div className="container-fluid jan-p0 jan-recommend ">
+        <div className="jan-recommend-wrap">
+          <div className="container mt-5 flex-column jan-p0">
+            {/* 今日推薦 */}
+            <div className="row col-6">
+              <img alt="" src={recommend1} />
+            </div>
+            {/* 你一定要試試 */}
+            <div className="row col-6">
+              <img alt="" src={recommend2} />
+            </div>
           </div>
-          {/* 你一定要試試 */}
-          <div className="row col-6">
-            <img alt="" src={recommend2} />
+
+          {/* 商品卡片 */}
+          <div className="mt-5 container jan-recommend-cards d-flex justify-content-around">
+            {/* <Card1 /> */}
+            <div className="row">
+              <div className="jan-card-container">
+                {/* item圖片 */}
+                <section className="jan-card-img-warp">
+                  <a href="/" className="jan-card-link">
+                    <img alt="" className="jan-card-img"></img>
+                  </a>
+                </section>
+
+                <section className="jan-card-info-warp d-flex justify-content-center">
+                  <div className="jan-card-info d-flex flex-column">
+                    <h3>慢煮嫩雞胸-蒜味香草</h3>
+                    {/* 分隔線 */}
+                    <section>
+                      <div className="jan-card-hr d-flex flex-column justify-content-center align-items-center">
+                        <div className="mt-3  mb-2 jan-card-star-warp">
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                        </div>
+                        <span className="d-flex justify-content-center">
+                          <p>232 則評論 </p>
+                          <p>250 已購買</p>
+                        </span>
+                      </div>
+                    </section>
+                  </div>
+                </section>
+              </div>
+            </div>
+            {/* <Card2 /> */}
+            <div className="row">
+              <div className="jan-card-container">
+                {/* item圖片 */}
+                <section className="jan-card-img-warp">
+                  <a href="/" className="jan-card-link">
+                    <img alt="" className="jan-card-img"></img>
+                  </a>
+                </section>
+
+                <section className="jan-card-info-warp d-flex justify-content-center">
+                  <div className="jan-card-info d-flex flex-column">
+                    <h3>慢煮嫩雞胸-蒜味香草</h3>
+                    {/* 分隔線 */}
+                    <section>
+                      <div className="jan-card-hr d-flex flex-column justify-content-center align-items-center">
+                        <div className="mt-3  mb-2 jan-card-star-warp">
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                        </div>
+                        <span className="d-flex justify-content-center">
+                          <p>232 則評論 </p>
+                          <p>250 已購買</p>
+                        </span>
+                      </div>
+                    </section>
+                  </div>
+                </section>
+              </div>
+            </div>
+            {/* Card3 */}
+            <div className="row">
+              <div className="jan-card-container">
+                {/* item圖片 */}
+                <section className="jan-card-img-warp">
+                  <a href="/" className="jan-card-link">
+                    <img alt="" className="jan-card-img"></img>
+                  </a>
+                </section>
+
+                <section className="jan-card-info-warp d-flex justify-content-center">
+                  <div className="jan-card-info d-flex flex-column">
+                    <h3>慢煮嫩雞胸-蒜味香草</h3>
+                    {/* 分隔線 */}
+                    <section>
+                      <div className="jan-card-hr d-flex flex-column justify-content-center align-items-center">
+                        <div className="mt-3  mb-2 jan-card-star-warp">
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                        </div>
+                        <span className="d-flex justify-content-center">
+                          <p>232 則評論 </p>
+                          <p>250 已購買</p>
+                        </span>
+                      </div>
+                    </section>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+
+          {/* 商品列表按鈕 */}
+          <div className="mt-5 container jan-index-button d-flex justify-content-center">
+            <Button className="button-btn" text="商品列表" />
           </div>
         </div>
-        <div className="container jan-recommend-cards">
-          <div className="row"></div>
-          <div className="row"></div>
-          <div className="row"></div>
-        </div>
-        <div className="jan-recommend-button"></div>
       </div>
 
       {/* 專屬優惠 */}
       <div className="container-fluid jan-p0  jan-discount">
         <div className="container jan-p0">
-          <div className="row jan-p0 mt-5 d-flex justify-content-center">
+          <div className="row jan-p0  d-flex justify-content-center">
             {/* 專屬優惠標題 */}
             <div className="d-flex align-items-center">
               <img alt="" src={titleLeft} />
               <p className="jan-feature-title">專屬優惠</p>
               <img alt="" src={titleRight} />
+            </div>
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <div class="img-demo">
+                    <img src="./imgs/product1.jpg" alt="" />
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="img-wrap d-flex">
+                    <div class="img-row">
+                      <img src="" alt="" />
+                    </div>
+                    <div class="img-row">
+                      <img src="" alt="" />
+                    </div>
+                    <div class="img-row">
+                      <img src="" alt="" />
+                    </div>
+                    <div class="img-row">
+                      <img src="" alt="" />
+                    </div>
+                    <div class="img-row">
+                      <img src="" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -180,8 +326,8 @@ function JanIndex(props) {
 
       {/* 地圖 */}
       <div className="container-fluid jan-p0 jan-map mt-5">
-        {/* 表格 */}
-        <div className="container jan-p0 jan-map-form">
+        {/* 橘色底色 */}
+        <div className="container flex-column jan-p0 jan-map-form">
           {/* 標題 */}
           <div className="container mt-5 flex-column jan-p0 jan-map-title">
             <br />
@@ -194,12 +340,21 @@ function JanIndex(props) {
               <img alt="" src={map2} />
             </div>
           </div>
-          <div className="container">
-            <div className="row"></div>
-          </div>
-          <div className="container jan-map-buttons">
-            <div className="row 5"></div>
-            <div className="row 5"></div>
+          {/* 表單 */}
+          <div className="container jan-p0 jan-adress-form">
+            <div className="container jan-p0">
+              <p>所在地點，符合滿</p>
+              <p>元免運費</p>
+              <p>最近的自取門市是</p>
+              <div className="jan-map-buttons d-flex justify-content-around">
+                <div className="row jan-index-button">
+                  <Button className="button-btn " text="商品列表" />
+                </div>
+                <div className="row jan-index-button">
+                  <Button className="button-btn " text="揪團訂購" />
+                </div>
+              </div>
+            </div>
           </div>
 
           {/* 地圖 */}
@@ -210,10 +365,46 @@ function JanIndex(props) {
       {/* 好文推薦 */}
       <div className="container-fluid jan-p0 jan-article">
         <div className="container jan-p0 d-flex justify-content-center">
+          {/* 標題 */}
           <div className="d-flex align-items-center">
             <img alt="" src={titleLeft} />
             <p className="jan-feature-title">好文推薦</p>
             <img alt="" src={titleRight} />
+          </div>
+
+          <div>
+            <div className="row">
+              <div className="jan-card-container">
+                {/* item圖片 */}
+                <section className="jan-card-img-warp">
+                  <a href="/" className="jan-card-link">
+                    <img alt="" className="jan-card-img"></img>
+                  </a>
+                </section>
+
+                <section className="jan-card-info-warp d-flex justify-content-center">
+                  <div className="jan-card-info d-flex flex-column">
+                    <h3>慢煮嫩雞胸-蒜味香草</h3>
+                    {/* 分隔線 */}
+                    <section>
+                      <div className="jan-card-hr d-flex flex-column justify-content-center align-items-center">
+                        <div className="mt-3  mb-2 jan-card-star-warp">
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                          <img alt="" className="jan-card-star" src={starO} />
+                        </div>
+                        <span className="d-flex justify-content-center">
+                          <p>232 則評論 </p>
+                          <p>250 已購買</p>
+                        </span>
+                      </div>
+                    </section>
+                  </div>
+                </section>
+              </div>
+            </div>
           </div>
         </div>
       </div>
