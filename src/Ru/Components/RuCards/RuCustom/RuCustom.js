3a461dafdd167accd796ef53a785a87e2939490a
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2020-10-30 22:03:23 +0800

    20201030 10:02p.m. 客製化便當容器骨架

--- src/Ru/Components/RuCards/RuCustom/RuCustom.js
+++ src/Ru/Components/RuCards/RuCustom/RuCustom.js
@@ -21,7 +21,33 @@ function RuCustom() {
           <div className="ru-drop-container">
             <div className="ru-drop-warp">
               <div className="ru-box-container"></div>
-              <div className="ru-detail-container"></div>
+              <div className="ru-detail-container">
+                <div className="ru-switchBtn-container"></div>
+                <div className="ru-info-container">
+                  <div className="ru-info-item-container">
+                    <div className="ru-info-item-warp">
+                      <div className="ru-info-item">
+                        <div className="ru-category-container">
+                          <div className="ru-category">副食</div>
+                        </div>
+                        <div className="ru-selectionName">香甜白飯</div>
+                        <div className="ru-number">$10</div>
+                      </div>
+                      <div className="ru-info-item"></div>
+                      <div className="ru-info-item"></div>
+                      <div className="ru-info-item"></div>
+                      <div className="ru-info-item"></div>
+                      <div className="ru-info-item"></div>
+                    </div>
+                  </div>
+                  <div className="ru-info-total-container">
+                    <div className="ru-info-total"></div>
+                  </div>
+                </div>
+                <div className="ru-checkout-container">
+                  <div className="ru-checkout-warp"></div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="ru-drag-container">
@@ -29,8 +55,20 @@ function RuCustom() {
               <RuArrowLeft />
             </span>
             <div className="ru-drag-warp">
-              <div className="ru-selection-container"></div>
-              <div className="ru-species-container"></div>
+              <div className="ru-selection-container">
+                <div className="ru-selection-warp"></div>
+              </div>
+              <div className="ru-species-container">
+                {/* 副食 / 主食 / 配菜 / 蛋 的元件 s*/}
+                <div className="ru-species-warp">
+                  <div className="ru-species ru-species1">AAA</div>
+                  <div className="ru-species ru-species2">BBB</div>
+                  <div className="ru-species ru-species3">CCC</div>
+                  <div className="ru-species ru-species4">DDD</div>
+                  <div className="ru-species ru-species5">EEE</div>
+                </div>
+                {/*  副食 / 主食 / 配菜 / 蛋 的元件 e*/}
+              </div>
             </div>
             <span>
               <RuArrowRight />
