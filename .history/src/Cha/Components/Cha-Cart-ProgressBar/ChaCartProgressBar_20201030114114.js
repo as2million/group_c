41cdb03a1336d74aa3522f7e9bde4fa0d90de31f
chaoting <02115214@gm.scu.edu.tw>
2020-10-30 13:32:05 +0800

    20201030 01:30p.m 進度條

---
+++ .history/src/Cha/Components/Cha-Cart-ProgressBar/ChaCartProgressBar_20201030114114.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+// import RightArrowWhite from 'Cha/Components/Cha-Cart-ProgressBar/Images/right-arrow-white.svg'
+import './ChaCartProgressBar.scss'
+import { Link } from 'react-router-dom'
+
+window.addEventListener('scroll', () => {
+  moveBar('#e74c3c')
+})
+
+// main code
+function moveBar(color = '#0984e3') {
+  let bar = document.querySelector('.cha-progress-bar')
+  let w = Math.ceil(
+    (Math.round(window.pageYOffset) /
+      (document.body.scrollHeight - window.innerHeight)) *
+      100
+  )
+  bar.style.width = w + '%'
+  bar.style.background = color
+}
+
+function ChaCartProgressBar(props) {
+  return (
+    <>
+      <div className="cha-progress-bar"></div>
+      <div className="cha-progress-bar-"></div>
+      <div className="cha-progress-bar-arrow-header">
+        <Link to="/">
+          <div className="cha-right-arrow"></div>
+        </Link>
+        <div className="cha-progress-bar-header">建立訂單</div>
+      </div>
+    </>
+  )
+}
+export default ChaCartProgressBar
