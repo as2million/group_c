cc3e5bb69d3022d049877e70b6e3df41200ce81e
tseng-yc <janice8819@gmail.com>
2020-10-30 15:58:05 +0800

    20201030 03:57p.m index.js切換

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import JanAppTest from './JanAppTest';
 import * as serviceWorker from './serviceWorker';
-import ClaudiaAppTest from './ClaudiaAppTest'
+// import ClaudiaAppTest from './ClaudiaAppTest'
 // import JessAppTest from './JessAppTest'
-import IrisAppTest from './IrisAppTest';
+// import IrisAppTest from './IrisAppTest';
 
 // import ChaAppTest from './ChaAppTest';
 // import RuAppTest from './RuAppTest';
 ReactDOM.render(
   <React.StrictMode>
-    <ClaudiaAppTest />
-    {/* <JanAppTest /> */}
+    {/* <ClaudiaAppTest /> */}
+    <JanAppTest />
 
     {/* <JessAppTest /> */}
-    <IrisAppTest />
+    {/* <IrisAppTest /> */}
 
     {/* <ChaAppTest /> */}
     {/* <RuAppTest /> */}
