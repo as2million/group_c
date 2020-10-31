3ba195b55fc7db63624ba06ab41f90146a5e1e6a
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2020-10-31 10:57:07 +0800

    20201031 10:56a.m. 客製化切版 填上詳細資訊

--- src/Ru/Components/RuCards/RuCustom/RuCustom.js
+++ src/Ru/Components/RuCards/RuCustom/RuCustom.js
@@ -25,23 +25,73 @@ function RuCustom() {
                 <div className="ru-switchBtn-container"></div>
                 <div className="ru-info-container">
                   <div className="ru-info-item-container">
-                    <div className="ru-info-item-warp">
-                      <div className="ru-info-item">
+                    {/* 詳細資料品項1 s */}
+                    <ul className="ru-info-item-warp">
+                      <li className="ru-info-item ru-info-item1">
                         <div className="ru-category-container">
-                          <div className="ru-category">副食</div>
+                          <p className="ru-category">副食</p>
                         </div>
-                        <div className="ru-selectionName">香甜白飯</div>
-                        <div className="ru-number">$10</div>
-                      </div>
-                      <div className="ru-info-item"></div>
-                      <div className="ru-info-item"></div>
-                      <div className="ru-info-item"></div>
-                      <div className="ru-info-item"></div>
-                      <div className="ru-info-item"></div>
-                    </div>
+                        <p className="ru-selectionName">香甜白飯</p>
+                        <p className="ru-number">$10</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+
+                      {/* 詳細資料品項1 s */}
+                      <li className="ru-info-item ru-info-item2">
+                        <div className="ru-category-container">
+                          <p className="ru-category">主食</p>
+                        </div>
+                        <p className="ru-selectionName">慢煮嫩雞胸-蒜味香草</p>
+                        <p className="ru-number">$55</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+
+                      {/* 詳細資料品項1 s */}
+                      <li className="ru-info-item ru-info-item3">
+                        <div className="ru-category-container">
+                          <p className="ru-category">配菜</p>
+                        </div>
+                        <p className="ru-selectionName">清炒高麗菜</p>
+                        <p className="ru-number">$10</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+
+                      {/* 詳細資料品項1 s */}
+                      <li className="ru-info-item ru-info-item4">
+                        <div className="ru-category-container">
+                          <p className="ru-category">配菜</p>
+                        </div>
+                        <p className="ru-selectionName">綠色嫩花椰</p>
+                        <p className="ru-number">$10</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+
+                      {/* 詳細資料品項1 s */}
+                      <li className="ru-info-item ru-info-item5">
+                        <div className="ru-category-container">
+                          <p className="ru-category">配菜</p>
+                        </div>
+                        <p className="ru-selectionName">黃金玉米</p>
+                        <p className="ru-number">$15</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+
+                      {/* 詳細資料品項1 s */}
+                      <li className="ru-info-item ru-info-item6">
+                        <div className="ru-category-container">
+                          <p className="ru-category">蛋</p>
+                        </div>
+                        <p className="ru-selectionName">水煮蛋</p>
+                        <p className="ru-number">$10</p>
+                      </li>
+                      {/* 詳細資料品項1 e */}
+                    </ul>
                   </div>
                   <div className="ru-info-total-container">
-                    <div className="ru-info-total"></div>
+                    <div className="ru-info-total-warp">
+                    <h3>總金額</h3>
+                    <p>$110</p>
+                    </div>
                   </div>
                 </div>
                 <div className="ru-checkout-container">
