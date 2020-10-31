67a32e8988926591fb73e760556e600266b561d9
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-10-31 15:22:24 +0800

    20201031 03:21pm make beastie point page

--- src/Iris/Components/IrisBeastiePointSect/IrisBeastiePointSect.js
+++ src/Iris/Components/IrisBeastiePointSect/IrisBeastiePointSect.js
@@ -5,6 +5,11 @@ import { ReactComponent as WaveLine } from './Images/wave_line.svg';
 import { ReactComponent as BesatieBook } from './Images/beastie_book.svg';
 import { ReactComponent as BesatieCoin } from './Images/beastie_coin.svg';
 import { ReactComponent as BesatieQ } from './Images/beastie_q.svg';
+import { ReactComponent as VerticalLine } from './Images/vertical_line.svg';
+import { ReactComponent as PencilIcon } from './Images/pencil_icon.svg';
+import { ReactComponent as BeastieCoupon20 } from './Images/beastie_coupon20.svg';
+import { ReactComponent as BeastieCoupon20Grey } from './Images/beastie-coupon20-grey.svg';
+import { ReactComponent as CouponVerticalLine } from './Images/coupon_vertical_line.svg';
 
 function IrisBeastiePointSect() {
   return (
@@ -12,11 +17,82 @@ function IrisBeastiePointSect() {
       <div className="container col-9">
         {/* <div className="row justify-content-center"> */}
         <div className="row justify-content-center iris-content-title-container ">
-          <h2 className="iris-profile-title">怪獸幣</h2>
+          <h2 className="iris-comment-title">怪獸幣</h2>
           <WaveLine />
-          <BesatieBook />
-          <BesatieQ />
-          <BesatieCoin />
+        </div>
+
+        <div className="iris-beastie-content-container ">
+          <div className="iris-beastie-icons-container d-flex  justify-content-center">
+            <div className="iris-icon-box d-flex align-items-center">
+              <BesatieCoin />
+              <p>怪獸幣總資產</p>
+            </div>
+            <div className="iris-beastie-coin">480</div>
+
+            <VerticalLine />
+
+            <div className="iris-beastie-box ">
+              <div className="iris-beastie-name-box d-flex align-items-center justify-content-center">
+                <PencilIcon />
+                &nbsp;&nbsp;
+                <div className="iris-beastie-name">小Q怪</div>
+              </div>
+              <BesatieQ />
+            </div>
+
+            <VerticalLine />
+
+            <div className="iris-icon-box d-flex align-items-center ">
+              <BesatieBook />
+              <p>遊戲規則</p>
+            </div>
+          </div>
+
+          <div className="iris-coupon-container">
+            <div className="iris-coupon-box d-flex align-items-center ">
+              <div className="iris-coupon-icon">
+                <BeastieCoupon20 />
+              </div>
+              <CouponVerticalLine />
+              <div className="iris-coupon-info d-flex flex-column">
+                <div className="iris-coupon-title">20元怪獸幣</div>
+                <div>
+                  <div className="iris-coupon-issue">
+                    <span>領取時間:</span>
+                    <span> 2020/09/20 13:32</span>
+                  </div>
+                  <div className="iris-coupon-due">
+                    <span>使用期限:</span>
+                    <span>2020/10/20 23:59 </span>
+                    <span>前</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* due */}
+            <div className="iris-coupon-box d-flex align-items-center ">
+              <div className="iris-coupon-icon">
+                <BeastieCoupon20Grey />
+              </div>
+              <CouponVerticalLine />
+              <div className="iris-coupon-info d-flex flex-column">
+                <div className="iris-coupon-title-grey">20元怪獸幣</div>
+                <div>
+                  <div className="iris-coupon-issue-grey">
+                    <span>領取時間:</span>
+                    <span> 2020/09/20 13:32</span>
+                  </div>
+                  <div className="iris-coupon-due-grey">
+                    <span>使用期限:</span>
+                    <span>2020/10/20 23:59 </span>
+                    <span>前</span>
+                  </div>
+                </div>
+                <div className="iris-coupon-due-text">已失效</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/Iris/Components/IrisDataEditSect/IrisDataEditSect.js
+++ src/Iris/Components/IrisDataEditSect/IrisDataEditSect.js
@@ -14,51 +14,54 @@ function IrisDataEditSect() {
         <div className="row justify-content-center iris-content-title-container ">
           <h2 className="iris-profile-title">會員資料修改</h2>
           <WaveLine />
-          <h6 className="iris-profile-note">
-            ※ 部分資料以 * 或隱藏處理，保護您的個人隱私
-          </h6>
-          <form className="iris-form-adjust">
-            <div className="d-flex  align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box testtest">姓氏</div>
+          <div />
+          <div className="row justify-content-center">
+            <h6 className="iris-profile-note">
+              ※ 部分資料以 * 或隱藏處理，保護您的個人隱私
+            </h6>
+            <form className="iris-form-adjust">
+              <div className="d-flex  align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box testtest">姓氏</div>
 
-              <InputH40 type="text" placeholder="" />
+                <InputH40 type="text" placeholder="" />
 
-              <div className="iris-input-box">名字</div>
+                <div className="iris-input-box">名字</div>
 
-              <InputH40 type="text" placeholder="" />
-            </div>
-            <div className="d-flex noF-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">生日</div>
+                <InputH40 type="text" placeholder="" />
+              </div>
+              <div className="d-flex noF-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">生日</div>
 
-              <InputH40 type="text" placeholder="" />
+                <InputH40 type="text" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">手機</div>
+                <InputH40 type="text" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">密碼</div>
+                <InputH40 type="password" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">新密碼</div>
+                <InputH40 type="password" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">確認新密碼</div>
+                <InputH40 type="password" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">信箱</div>
+                <InputH40 type="text" placeholder="" />
+              </div>
+              <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
+                <div className="iris-input-box">配送地址</div>
+                <InputH40 type="text" placeholder="" />
+              </div>
+            </form>
+            <div className="iris-profile-button">
+              <Button className="button-btn" text="更新個人資料" />
             </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">手機</div>
-              <InputH40 type="text" placeholder="" />
-            </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">密碼</div>
-              <InputH40 type="password" placeholder="" />
-            </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">新密碼</div>
-              <InputH40 type="password" placeholder="" />
-            </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">確認新密碼</div>
-              <InputH40 type="password" placeholder="" />
-            </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">信箱</div>
-              <InputH40 type="text" placeholder="" />
-            </div>
-            <div className="d-flex no-wrap align-items-center iris-profile-item-wrapper">
-              <div className="iris-input-box">配送地址</div>
-              <InputH40 type="text" placeholder="" />
-            </div>
-          </form>
-          <div className="iris-profile-button">
-            <Button className="button-btn" text="更新個人資料" />
           </div>
         </div>
       </div>
