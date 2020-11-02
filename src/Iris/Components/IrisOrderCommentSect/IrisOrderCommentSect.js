9c5115329ab76370f0d0347078f2ce4a797500c4
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-11-02 12:51:40 +0800

    20201102 12:51pm make order comment sect edit

--- src/Iris/Components/IrisOrderCommentSect/IrisOrderCommentSect.js
+++ src/Iris/Components/IrisOrderCommentSect/IrisOrderCommentSect.js
@@ -3,8 +3,97 @@ import './IrisOrderCommentSect.scss';
 import { ReactComponent as WaveLine } from './Images/wave_line.svg';
 import { ReactComponent as IrisMemberLine } from './Images/iris_member_line.svg';
 import star from './Images/star.svg';
+import IrisTextArea from './IrisTextArea/IrisTextArea';
+// import InputH40 from './../../../Share/Components/Input/InputH40';
 
 function IrisUserCommentSect() {
+  // 改留言
+  const changeComment = (e) => {
+    // 測試: 得到top parent的id
+    // alert(e.target.parentNode.parentNode.parentNode.id);
+
+    // 留言,編輯,刪除的字消失，出現輸入框
+    const originalComment = document.querySelector('.iris-comment-text');
+    document.querySelector('.iris-text-area .iris-textarea').value =
+      originalComment.innerText;
+    console.log(originalComment.innerText);
+    const commentInput = document.querySelector('.iris-text-area');
+    const editAndDelete = document.querySelector('.iris-comment-edit-delete');
+    const commentUpdate = document.querySelector('.iris-comment-update');
+    originalComment.style.display = 'none';
+    commentInput.style.display = 'block';
+    editAndDelete.style.display = 'none';
+    commentUpdate.style.display = 'block';
+  };
+
+  // 輸入框消失，更新留言
+  const doCommentUpdate = () => {
+    const originalComment = document.querySelector('.iris-comment-text');
+    const commentInput = document.querySelector('.iris-text-area');
+    const editAndDelete = document.querySelector('.iris-comment-edit-delete');
+    const commentUpdate = document.querySelector('.iris-comment-update ');
+    console.log(commentInput.value);
+    originalComment.innerText = document.querySelector(
+      '.iris-text-area .iris-textarea'
+    ).value;
+    originalComment.style.display = 'block';
+    commentInput.style.display = 'none';
+    editAndDelete.style.display = 'block';
+    commentUpdate.style.display = 'none';
+  };
+  // 留言框
+  const commentDisplay = (
+    <>
+      <div className="iris-comment-box d-flex" id="comment1">
+        <div className="iris-comment-img-warpper">
+          <img className="iris-comment-img comment-img-1"></img>
+        </div>
+        <div className="iris-comment-section-wrapper">
+          <div className="iris-comment-text-wrapper d-flex">
+            <div>慢煮嫩雞胸-蒜味香草</div>&nbsp;&nbsp;
+            <div className="card-star-warp">
+              <img className="card-star" src={star} />
+              <img className="card-star" src={star} />
+              <img className="card-star" src={star} />
+              <img className="card-star" src={star} />
+              <img className="card-star" src={star} />
+            </div>
+          </div>
+          <div className="iris-comment-text">
+            方便又美味 上班吃什麼都不煩惱了
+          </div>
+          <div className="iris-text-area">
+            <IrisTextArea
+            // value={document.querySelector('.iris-comment-text').innerText}
+            />
+            {/* <InputH40 /> */}
+          </div>
+          <div className="iris-comment-edit-delete">
+            <span
+              className="comment-edit"
+              onClick={(e) => {
+                changeComment(e);
+              }}
+            >
+              編輯
+            </span>
+            &nbsp;<span> | </span>&nbsp;
+            <span className="comment-delete">刪除</span>
+          </div>
+          <span
+            className="iris-comment-update"
+            onClick={() => {
+              doCommentUpdate();
+            }}
+          >
+            確認
+          </span>
+          <div className="iris-comment-date">2020/09/26</div>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <>
       <div className="container col-9">
@@ -18,59 +107,11 @@ function IrisUserCommentSect() {
           </h6>
           {/* <IrisMemberLine /> */}
           <div className="iris-member-line"></div>
-          <div className="iris-comment-box d-flex">
-            <div className="iris-comment-img-warpper">
-              <img className="iris-comment-img comment-img-1"></img>
-            </div>
-            <div className="iris-comment-section-wrapper">
-              <div className="iris-comment-text-wrapper d-flex">
-                <div>慢煮嫩雞胸-蒜味香草</div>&nbsp;&nbsp;
-                <div className="card-star-warp">
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                </div>
-              </div>
-              <div className="iris-comment-text">
-                方便又美味 上班吃什麼都不煩惱了
-              </div>
-              <div className="iris-comment-edit-delete">
-                <span className="comment-edit">編輯</span>
-                &nbsp;<span> | </span>&nbsp;
-                <span className="comment-delete">刪除</span>
-              </div>
-              <div className="iris-comment-date">2020/09/26</div>
-            </div>
-          </div>
+          {commentDisplay}
           <div className="iris-member-line"></div>
-          <div className="iris-comment-box d-flex">
-            <div className="iris-comment-img-warpper">
-              <img className="iris-comment-img comment-img-2"></img>
-            </div>
-            <div className="iris-comment-section-wrapper">
-              <div className="iris-comment-text-wrapper d-flex">
-                <div>慢煮嫩雞胸-蒜味香草</div>&nbsp;&nbsp;
-                <div className="card-star-warp">
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                </div>
-              </div>
-              <div className="iris-comment-text">
-                方便又美味 上班吃什麼都不煩惱了
-              </div>
-              <div className="iris-comment-edit-delete">
-                <span className="comment-edit">編輯</span>
-                &nbsp; <span> | </span>&nbsp;
-                <span className="comment-delete">刪除</span>
-              </div>
-              <div className="iris-comment-date">2020/09/26</div>
-            </div>
-          </div>
+          {commentDisplay}
+          <div className="iris-member-line"></div>
+          {commentDisplay}
         </div>
       </div>
     </>
