5b6c8abb9e65d370f31b6b30e6cc5e13571f4a53
chaoting <02115214@gm.scu.edu.tw>
2020-11-04 16:34:49 +0800

    20201104 04:34p.m. 修繕滾動進度條、計數器跑版

--- .history/src/Cha/Components/Cha-Cart-ProgressBar/ChaCartProgressBar_20201104152210.js
+++ .history/src/Cha/Components/Cha-Cart-ProgressBar/ChaCartProgressBar_20201104152210.js
@@ -25,18 +25,17 @@ function ChaCartProgressBar(props) {
   return (
     <>
       <div className="cha-progress-bar"></div>
-
-      <div className="cha-progress-bar-arrow-header">
-        <Link to="/">
-          <div className="cha-right-arrow"></div>
-        </Link>
-        <div className="cha-progress-bar-header">建立訂單</div>
-      </div>
       <div className="cha-progress-bar-yellow">
         <div className="cha-progress-bar-arrow-header-white">
           <div className="cha-right-arrow"></div>
           <div className="cha-progress-bar-header-fff">建立訂單</div>
         </div>
+      </div>
+      <div className="cha-progress-bar-arrow-header">
+        <Link to="/">
+          <div className="cha-right-arrow"></div>
+        </Link>
+        <div className="cha-progress-bar-header">建立訂單</div>
       </div>
     </>
   );
