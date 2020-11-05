629ee05faa1927b52b146a58d9e64008f46c36cb
Claudia <sydeng102@gmail.com>
2020-11-05 14:34:03 +0800

    20201105 0233p.m fix map buttons

--- src/Claudia/Components/ClaudiaIndexCardList/ClaudiaIndexCardList.js
+++ src/Claudia/Components/ClaudiaIndexCardList/ClaudiaIndexCardList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ClaudiaIndexCardList.scss'
 import Cross from './Images/cross.svg';
 
-function ClaudiaIndexCardList() {
+function ClaudiaIndexCardList(props) {
+
+    const { city, data } = props;
+
 
     //show farm intro
 
@@ -17,20 +20,75 @@ function ClaudiaIndexCardList() {
         // console.log('TorF', document.getElementsByClassName('claudia-index-card-intro-box')[0].style.display)
     }
 
+    //add buttons
+    if (data) {
+        // console.log('data', data);
+        // console.log('data', data.length);
+
+
+        document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].innerHTML = ''
+
+        for (let i = 0; i < data.length; i++) {
+            const temp = `
+            <button>
+                <div>
+                    <p><b>${data[i].farm}</b></p>
+                </div>
+            </button>
+            `
+            document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].innerHTML += temp;
+
+            //addClassName
+            document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].children[i].className += "claudia-index-card-list-box-button"
+            document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].children[i].children[0].className += "claudia-index-card-list-box-name-text"
+        }
+
+        //addEventListener
+        console.log('test', document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].children)
+        const buttons = document.getElementsByClassName('claudia-index-card-list-box-buttons')[0].children
+
+        for (let item of buttons) {
+            item.addEventListener('click', () => showFarmIntro())
+        }
+    }
+
     return (
         <>
             <div className="claudia-index-card-list-outside">
                 <div className="claudia-index-card-list-box">
-                    <h1 id="claudia-index-card-list-city"><b>台北市</b></h1>
+                    <h1 id="claudia-index-card-list-city"><b>{city}</b></h1>
                     <h2 id="claudia-index-card-list-text">點選農場名稱，看更多資訊～</h2>
-                    <button
-                        onClick={showFarmIntro}
-                        className="claudia-index-card-list-box-button">
-                        <div className="claudia-index-card-list-box-name-text">
-                            <p><b>小小城市農夫</b></p>
-                            <p><b>－台北內湖農驛棧有機農園</b></p>
-                        </div>
-                    </button>
+                    <div className="claudia-index-card-list-box-buttons">
+                        {/* <button
+                            onClick={showFarmIntro}
+                            className="claudia-index-card-list-box-button">
+                            <div className="claudia-index-card-list-box-name-text">
+                                <p><b>小小城市農夫－台北內湖農驛棧有機農園</b></p>
+                            </div>
+                        </button>
+                        <button
+                            onClick={showFarmIntro}
+                            className="claudia-index-card-list-box-button">
+                            <div className="claudia-index-card-list-box-name-text">
+                                <p><b>小小城市農夫－台北內湖農驛棧有機農園</b></p>
+                            </div>
+                        </button>
+                        <button
+                            onClick={showFarmIntro}
+                            className="claudia-index-card-list-box-button">
+                            <div className="claudia-index-card-list-box-name-text">
+                                <p><b>小小城市農夫－台北內湖農驛棧有機農園</b></p>
+                            </div>
+                        </button>
+                        <button
+                            onClick={showFarmIntro}
+                            className="claudia-index-card-list-box-button">
+                            <div className="claudia-index-card-list-box-name-text">
+                                <p><b>小小城市農夫－台北內湖農驛棧有機農園</b></p>
+                            </div>
+                        </button> */}
+                    </div>
+
                 </div>
             </div>
         </>
--- src/Claudia/Components/ClaudiaIndexContent/ClaudiaIndexContent.js
+++ src/Claudia/Components/ClaudiaIndexContent/ClaudiaIndexContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ClaudiaIndexContent.scss'
 import ClaudiaIndexMap from '../ClaudiaIndexMap/ClaudiaIndexMap';
 import ClaudiaIndexCardIntro from '../ClaudiaIndexCardIntro/ClaudiaIndexCardIntro';
@@ -6,13 +6,16 @@ import ClaudiaIndexCardList from '../ClaudiaIndexCardList/ClaudiaIndexCardList';
 
 function ClaudiaIndexContent() {
 
+    const [city, setCity] = useState('');
+    const [data, setData] = useState('');
+
     return (
         <>
             <div className="claudia-index-content">
                 <div className="claudia-index-content-container">
-                    <div className="claudia-index-map"><ClaudiaIndexMap /></div>
+                    <div className="claudia-index-map"><ClaudiaIndexMap setCity={setCity} setData={setData} /></div>
                     <div className="claudia-index-card">
-                        <ClaudiaIndexCardList />
+                        <ClaudiaIndexCardList city={city} data={data} />
                         <ClaudiaIndexCardIntro />
                     </div>
                 </div>
