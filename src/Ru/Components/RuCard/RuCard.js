5f23271a1ce20d145da7971f4742dbdb55ad1440
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2020-11-10 00:21:42 +0800

    20201110 12:21a.m. 商品列表get完成

--- src/Ru/Components/RuCard/RuCard.js
+++ src/Ru/Components/RuCard/RuCard.js
@@ -13,53 +13,71 @@ function RuCard(props) {
   // id 不同元件id => addCart-btn-n n為自訂數
   // parentId 不同元件父母id => addCart-btn-warp-n n為自訂數
   // imgId 產品圖片 => card-img-n n為1~9
-  const { title, comment, buy, price, cardMargin, id, parentId, imgId, handleAddCartNumber } = props
+  const {
+    title,
+    comment,
+    buy,
+    price,
+    cardMargin,
+    id,
+    parentId,
+    imgId,
+    handleAddCartNumber,
+  } = props
 
   return (
     <>
-      <div className="card-container" id={cardMargin}>
+      <div className="ru-card-container" id={cardMargin}>
         {/* item圖片s */}
-        <section className="card-img-warp">
-          <a href="" className="card-link">
-            <img className="card-img" id={imgId}></img>
+        <section className="ru-card-img-warp">
+          <a href="" className="ru-card-link">
+            <img
+              className="ru-card-img"
+              style={{
+                // "/" => 表示在public資料夾
+                backgroundImage: `url("/productImages/Bento/${imgId}.jpg")`,
+              }}
+            ></img>
           </a>
-          <div className="card-abs">
+          <div className="ru-card-abs">
             <RuAddFavorite />
           </div>
         </section>
         {/* item圖片e */}
         {/* item資訊s */}
-        <section className="card-info-warp">
-          <div className="card-none">
+        <section className="ru-card-info-warp">
+          <div className="ru-card-none">
             <h3>{price}</h3>
           </div>
-          <div className="card-info">
+          <div className="ru-card-info">
             <h2>{title}</h2>
             <section>
               <div>
-                <div className="card-star-warp">
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
-                  <img className="card-star" src={star} />
+                <div className="ru-card-star-warp">
+                  <img className="ru-card-star" src={star} />
+                  <img className="ru-card-star" src={star} />
+                  <img className="ru-card-star" src={star} />
+                  <img className="ru-card-star" src={star} />
+                  <img className="ru-card-star" src={star} />
                 </div>
                 <span>
-                  <p className="card-num card-commentNum">{comment} 則評論</p>
-                  <p className="card-num card-buyNum">{buy} 已購買</p>
+                  <p className="ru-card-num ru-card-commentNum">
+                    {comment} 則評論
+                  </p>
+                  <p className="ru-card-num ru-card-buyNum">{buy} 已購買</p>
                 </span>
               </div>
             </section>
           </div>
-          <div className="card-price">
+          <div className="ru-card-price">
             <h3>{price}</h3>
           </div>
         </section>
         {/* item資訊e */}
         {/* 加入購物車按鈕s */}
-        <section className="card-addCartWarp">
-          <div className="card-hr">
-            <RuAddCart 
+        <section className="ru-card-addCartWarp">
+          <div className="ru-card-hr">
+            <RuAddCart
               target={'target'}
               id={id}
               parentId={parentId}
--- src/Ru/Components/RuCards/RuBento/RuBento.js
+++ src/Ru/Components/RuCards/RuBento/RuBento.js
@@ -18,6 +18,9 @@ function RuBento(props) {
   const [itemWarp7, setItemWarp7] = useState(false)
   const [itemWarp8, setItemWarp8] = useState(false)
   const [itemWarp9, setItemWarp9] = useState(false)
+  const [itemWarp10, setItemWarp10] = useState(false)
+  const [itemWarp11, setItemWarp11] = useState(false)
+  const [itemWarp12, setItemWarp12] = useState(false)
   const [isShowNothing, setIsShowNothing] = useState(false)
   // 後端資料
   const [data, setData] = useState('')
@@ -40,15 +43,18 @@ function RuBento(props) {
     if (!data) {
       return
     }
-    let title1 = '慢煮嫩雞胸-蒜味香草'
-    let title2 = '慢煮嫩雞胸-中歐香料'
-    let title3 = '醬烤厚切1983黑豚'
-    let title4 = '熱帶火烤萊姆蝦'
-    let title5 = '麴塩五香松阪豬'
-    let title6 = '美式熟成烤牛肋條'
-    let title7 = '頂級熟成菲力牛排'
-    let title8 = '炙燒干貝'
-    let title9 = '會議雙拼組合'
+    let title1 = data[0].productname
+    let title2 =data[1].productname
+    let title3 = data[2].productname
+    let title4 = data[3].productname
+    let title5 = data[4].productname
+    let title6 = data[5].productname
+    let title7 = data[6].productname
+    let title8 = data[7].productname
+    let title9 = data[8].productname
+    let title10 = data[9].productname
+    let title11 = data[10].productname
+    let title12 = data[11].productname
     const $containerA = document.querySelector('.ru-itemWarp')
 
     // 第一次掛載DOM 與 每次state改變時 都會觸發
@@ -62,6 +68,9 @@ function RuBento(props) {
     setItemWarp7(true)
     setItemWarp8(true)
     setItemWarp9(true)
+    setItemWarp10(true)
+    setItemWarp11(true)
+    setItemWarp12(true)
     setIsShowNothing(false)
 
     if (title1.indexOf(searchInput) == -1) {
@@ -91,6 +100,15 @@ function RuBento(props) {
     if (title9.indexOf(searchInput) == -1) {
       setItemWarp9(false)
     }
+    if (title10.indexOf(searchInput) == -1) {
+      setItemWarp10(false)
+    }
+    if (title11.indexOf(searchInput) == -1) {
+      setItemWarp11(false)
+    }
+    if (title12.indexOf(searchInput) == -1) {
+      setItemWarp12(false)
+    }
     if (
       title1.indexOf(searchInput) == -1 &&
       title2.indexOf(searchInput) == -1 &&
@@ -100,7 +118,10 @@ function RuBento(props) {
       title6.indexOf(searchInput) == -1 &&
       title7.indexOf(searchInput) == -1 &&
       title8.indexOf(searchInput) == -1 &&
-      title9.indexOf(searchInput) == -1
+      title9.indexOf(searchInput) == -1 &&
+      title10.indexOf(searchInput) == -1 &&
+      title11.indexOf(searchInput) == -1 &&
+      title12.indexOf(searchInput) == -1
     ) {
       setIsShowNothing(true)
     }
@@ -123,10 +144,10 @@ function RuBento(props) {
                 title={data[0].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-1'}
-                parentId={'addCart-btn-warp-1'}
-                imgId={'card-img-1'}
+                price={data[0].price}
+                id={'ru-addCart-btn-1'}
+                parentId={'ru-addCart-btn-warp-1'}
+                imgId={data[0].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
@@ -136,23 +157,23 @@ function RuBento(props) {
                 title={data[1].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-2'}
-                parentId={'addCart-btn-warp-2'}
-                imgId={'card-img-2'}
+                price={data[1].price}
+                id={'ru-addCart-btn-2'}
+                parentId={'ru-addCart-btn-warp-2'}
+                imgId={data[1].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
             )}
             {itemWarp3 && (
               <RuCard
-                title={'醬烤厚切1983黑豚'}
+                title={data[2].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-3'}
-                parentId={'addCart-btn-warp-3'}
-                imgId={'card-img-3'}
+                price={data[2].price}
+                id={'ru-addCart-btn-3'}
+                parentId={'ru-addCart-btn-warp-3'}
+                imgId={data[2].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
@@ -160,39 +181,39 @@ function RuBento(props) {
 
             {itemWarp4 && (
               <RuCard
-                title={'熱帶火烤萊姆蝦'}
+                title={data[3].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-4'}
-                parentId={'addCart-btn-warp-4'}
-                imgId={'card-img-4'}
+                price={data[3].price}
+                id={'ru-addCart-btn-4'}
+                parentId={'ru-addCart-btn-warp-4'}
+                imgId={data[3].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
             )}
             {itemWarp5 && (
               <RuCard
-                title={'麴塩五香松阪豬'}
+                title={data[4].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-5'}
-                parentId={'addCart-btn-warp-5'}
-                imgId={'card-img-5'}
+                price={data[4].price}
+                id={'ru-addCart-btn-5'}
+                parentId={'ru-addCart-btn-warp-5'}
+                imgId={data[4].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
             )}
             {itemWarp6 && (
               <RuCard
-                title={'美式熟成烤牛肋條'}
+                title={data[5].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-6'}
-                parentId={'addCart-btn-warp-6'}
-                imgId={'card-img-6'}
+                price={data[5].price}
+                id={'ru-addCart-btn-6'}
+                parentId={'ru-addCart-btn-warp-6'}
+                imgId={data[5].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
@@ -200,39 +221,78 @@ function RuBento(props) {
 
             {itemWarp7 && (
               <RuCard
-                title={'頂級熟成菲力牛排'}
+                title={data[6].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-7'}
-                parentId={'addCart-btn-warp-7'}
-                imgId={'card-img-7'}
+                price={data[6].price}
+                id={'ru-addCart-btn-7'}
+                parentId={'ru-addCart-btn-warp-7'}
+                imgId={data[6].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
             )}
             {itemWarp8 && (
               <RuCard
-                title={'炙燒干貝'}
+                title={data[7].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-8'}
-                parentId={'addCart-btn-warp-8'}
-                imgId={'card-img-8'}
+                price={data[7].price}
+                id={'ru-addCart-btn-8'}
+                parentId={'ru-addCart-btn-warp-8'}
+                imgId={data[7].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
             )}
             {itemWarp9 && (
               <RuCard
-                title={'會議雙拼組合'}
+                title={data[8].productname}
                 comment={'1180'}
                 buy={'234'}
-                price={'$130'}
-                id={'addCart-btn-9'}
-                parentId={'addCart-btn-warp-9'}
-                imgId={'card-img-9'}
+                price={data[8].price}
+                id={'ru-addCart-btn-9'}
+                parentId={'ru-addCart-btn-warp-9'}
+                imgId={data[8].img_id}
+                handleAddCartNumber={handleAddCartNumber}
+                currentUser={currentUser}
+              />
+            )}
+             {itemWarp10 && (
+              <RuCard
+                title={data[9].productname}
+                comment={'1180'}
+                buy={'234'}
+                price={data[9].price}
+                id={'ru-addCart-btn-10'}
+                parentId={'ru-addCart-btn-warp-10'}
+                imgId={data[9].img_id}
+                handleAddCartNumber={handleAddCartNumber}
+                currentUser={currentUser}
+              />
+            )}
+             {itemWarp11 && (
+              <RuCard
+                title={data[10].productname}
+                comment={'1180'}
+                buy={'234'}
+                price={data[10].price}
+                id={'ru-addCart-btn-11'}
+                parentId={'ru-addCart-btn-warp-11'}
+                imgId={data[10].img_id}
+                handleAddCartNumber={handleAddCartNumber}
+                currentUser={currentUser}
+              />
+            )}
+             {itemWarp12 && (
+              <RuCard
+                title={data[11].productname}
+                comment={'1180'}
+                buy={'234'}
+                price={data[11].price}
+                id={'ru-addCart-btn-12'}
+                parentId={'ru-addCart-btn-warp-12'}
+                imgId={data[11].img_id}
                 handleAddCartNumber={handleAddCartNumber}
                 currentUser={currentUser}
               />
@@ -255,19 +315,19 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-1'}
-              parentId={'addCart-btn-warp-1'}
-              imgId={'card-img-1'}
+              id={'ru-addCart-btn-1'}
+              parentId={'ru-addCart-btn-warp-1'}
+              imgId={'ru-card-img-1'}
             />
             <RuCard
               title={'慢煮嫩雞胸-中歐香料'}
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-2'}
-              parentId={'addCart-btn-warp-2'}
-              imgId={'card-img-2'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-2'}
+              parentId={'ru-addCart-btn-warp-2'}
+              imgId={'ru-card-img-2'}
+              cardMargin={'ru-card-margin'}
             />
           </div>
 
@@ -277,18 +337,18 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-3'}
-              parentId={'addCart-btn-warp-3'}
-              imgId={'card-img-3'}
+              id={'ru-addCart-btn-3'}
+              parentId={'ru-addCart-btn-warp-3'}
+              imgId={'ru-card-img-3'}
             />
             <RuCard
               title={'熱帶火烤萊姆蝦'}
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-4'}
-              parentId={'addCart-btn-warp-4'}
-              imgId={'card-img-4'}
+              id={'ru-addCart-btn-4'}
+              parentId={'ru-addCart-btn-warp-4'}
+              imgId={'ru-card-img-4'}
             />
           </div>
 
@@ -298,19 +358,19 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-5'}
-              parentId={'addCart-btn-warp-5'}
-              imgId={'card-img-5'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-5'}
+              parentId={'ru-addCart-btn-warp-5'}
+              imgId={'ru-card-img-5'}
+              cardMargin={'ru-card-margin'}
             />
             <RuCard
               title={'美式熟成烤牛肋條'}
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-6'}
-              parentId={'addCart-btn-warp-6'}
-              imgId={'card-img-6'}
+              id={'ru-addCart-btn-6'}
+              parentId={'ru-addCart-btn-warp-6'}
+              imgId={'ru-card-img-6'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp4">
@@ -319,19 +379,19 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-7'}
-              parentId={'addCart-btn-warp-7'}
-              imgId={'card-img-7'}
+              id={'ru-addCart-btn-7'}
+              parentId={'ru-addCart-btn-warp-7'}
+              imgId={'ru-card-img-7'}
             />
             <RuCard
               title={'炙燒干貝'}
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-8'}
-              parentId={'addCart-btn-warp-8'}
-              imgId={'card-img-8'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-8'}
+              parentId={'ru-addCart-btn-warp-8'}
+              imgId={'ru-card-img-8'}
+              cardMargin={'ru-card-margin'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp5">
@@ -340,18 +400,18 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-9'}
-              parentId={'addCart-btn-warp-9'}
-              imgId={'card-img-9'}
+              id={'ru-addCart-btn-9'}
+              parentId={'ru-addCart-btn-warp-9'}
+              imgId={'ru-card-img-9'}
             />
             <RuCard
               title={'這是用於排版的'}
               comment={'xxxx'}
               buy={'xxx'}
               price={'$XXX'}
-              id={''}
-              parentId={''}
-              imgId={'card-img-1'}
+              id={'ru-'}
+              parentId={'ru-'}
+              imgId={'ru-card-img-1'}
             />
           </div>
         </div>
@@ -371,9 +431,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-1'}
-              parentId={'addCart-btn-warp-1'}
-              imgId={'card-img-1'}
+              id={'ru-addCart-btn-1'}
+              parentId={'ru-addCart-btn-warp-1'}
+              imgId={'ru-card-img-1'}
             />
           </div>
 
@@ -383,10 +443,10 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-2'}
-              parentId={'addCart-btn-warp-2'}
-              imgId={'card-img-2'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-2'}
+              parentId={'ru-addCart-btn-warp-2'}
+              imgId={'ru-card-img-2'}
+              cardMargin={'ru-card-margin'}
             />
           </div>
 
@@ -396,9 +456,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-3'}
-              parentId={'addCart-btn-warp-3'}
-              imgId={'card-img-3'}
+              id={'ru-addCart-btn-3'}
+              parentId={'ru-addCart-btn-warp-3'}
+              imgId={'ru-card-img-3'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp4">
@@ -407,9 +467,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-4'}
-              parentId={'addCart-btn-warp-4'}
-              imgId={'card-img-4'}
+              id={'ru-addCart-btn-4'}
+              parentId={'ru-addCart-btn-warp-4'}
+              imgId={'ru-card-img-4'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp5">
@@ -418,10 +478,10 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-5'}
-              parentId={'addCart-btn-warp-5'}
-              imgId={'card-img-5'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-5'}
+              parentId={'ru-addCart-btn-warp-5'}
+              imgId={'ru-card-img-5'}
+              cardMargin={'ru-card-margin'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp6">
@@ -430,9 +490,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-6'}
-              parentId={'addCart-btn-warp-6'}
-              imgId={'card-img-6'}
+              id={'ru-addCart-btn-6'}
+              parentId={'ru-addCart-btn-warp-6'}
+              imgId={'ru-card-img-6'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp7">
@@ -441,9 +501,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-7'}
-              parentId={'addCart-btn-warp-7'}
-              imgId={'card-img-7'}
+              id={'ru-addCart-btn-7'}
+              parentId={'ru-addCart-btn-warp-7'}
+              imgId={'ru-card-img-7'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp8">
@@ -452,10 +512,10 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-8'}
-              parentId={'addCart-btn-warp-8'}
-              imgId={'card-img-8'}
-              cardMargin={'card-margin'}
+              id={'ru-addCart-btn-8'}
+              parentId={'ru-addCart-btn-warp-8'}
+              imgId={'ru-card-img-8'}
+              cardMargin={'ru-card-margin'}
             />
           </div>
           <div className="ru-itemWarp ru-itemWarp9">
@@ -464,9 +524,9 @@ function RuBento(props) {
               comment={'1180'}
               buy={'234'}
               price={'$130'}
-              id={'addCart-btn-9'}
-              parentId={'addCart-btn-warp-9'}
-              imgId={'card-img-9'}
+              id={'ru-addCart-btn-9'}
+              parentId={'ru-addCart-btn-warp-9'}
+              imgId={'ru-card-img-9'}
             />
           </div>
         </div>
