26936d6725ceebffce6a913958ff79d032fb0769
Claudia <sydeng102@gmail.com>
2020-11-10 10:59:25 +0800

    20201110 10:50a.m. develop push

--- src/App.js
+++ src/App.js
@@ -45,62 +45,6 @@ function App() {
 
         {/* 路由設定開始 */}
         <Switch>
-          {/* janice */}
-          {/* 放"page資料夾"內的元件 */}
-          {/* <Route exact path="/">
-            <JanIndex />
-          </Route> */}
-
-          {/* jess */}
-          {/* 放"page資料夾"內的元件 */}
-          {/* <Route exact path="/menu">
-            <JessMenu />
-          </Route>
-          <Route exact path="/bento/:id">
-            <JessBento />
-          </Route>
-          <Route exact path="/vegBox">
-            <JessVegBox />
-          </Route>
-
-          {/* iris */}
-          {/* 放"page資料夾"內的元件 */}
-          {/* <Route exact path="/memberUserprofile">
-            <IrisUserprofile />
-          </Route>
-          <Route exact path="/orderComment">
-            <IrisOrderComment />
-          </Route>
-          <Route exact path="/myFav">
-            <IrisMyFav />
-          </Route>
-          <Route exact path="/beastiePoint">
-            <IrisBeastiePoint />
-          </Route> */}
-
-          {/* ru */}
-          {/* 放"page資料夾"內的元件 */}
-          {/* <Route exact path="/productList">
-            <RuProudctList />
-          </Route>
-          <Route exact path="/productListSalad">
-            <RuProudctListSalad />
-          </Route>
-          <Route exact path="/productListCustom">
-            <RuProudctListCustom />
-          </Route> */}
-
-          {/* cha */}
-          {/* 放"page資料夾"內的元件 */}
-          {/* <Route exact path="/cart">
-            <ChaCart />
-          </Route>
-          <Route exact path="/groupOrder/groupOrderCreate">
-            <ChaGroupOrder />
-          </Route>
-          <Route exact path="/orderManagement">
-            <ChaOrderManagement />
-          </Route> */}
 
           {/* claudia */}
           {/* 放"page資料夾"內的元件 */}
