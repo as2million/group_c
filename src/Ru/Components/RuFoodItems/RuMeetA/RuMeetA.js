f5faaee8e136a75144d028bab2ba3f3070399d65
Ruru <k718198@gmail.com>
2020-11-11 17:37:16 +0800

    20201111 05:37p.m. 客製化 補品項((雞腿)

--- src/Ru/Components/RuFoodItems/RuMeetA/RuMeetA.js
+++ src/Ru/Components/RuFoodItems/RuMeetA/RuMeetA.js
@@ -3,6 +3,7 @@ import './Style.scss' // 單獨
 import '../Style.scss' // 共用
 import shrimp from './Images/shrimp.svg'
 import chickenBreast from './Images/chickenBreast.svg'
+import chickenLeg from './Images/chickenLeg.svg'
 
 // 網頁版 主食 選項
 function RuMeetA(props) {
@@ -38,6 +39,35 @@ function RuMeetA(props) {
         </div>
       </li>
       {/* 品項1 e*/}
+         {/* 品項2 s*/}
+         <li className="ru-species-item ru-species-item2" id="">
+        <div className="ru-species-img" id="ru-item2">
+          <img
+            src={chickenLeg}
+            className="ru-items ru-meet"
+            id="ru-meet-2"
+            draggable="true"
+          ></img>
+        </div>
+        <div className="ru-species-info">
+          <ul>
+            <li>
+              <h4>
+                {data[4].productName} <span>${data[4].price}</span>
+              </h4>
+            </li>
+            <li className="ru-species-calories">
+              熱量: {data[4].calories}大卡
+            </li>
+            <li className="ru-species-carbohydrates">
+              碳水化合物:{data[4].cabohydrate}g
+            </li>
+            <li className="ru-species-protein">蛋白質: {data[4].protein}g</li>
+            <li className="ru-species-fat">脂肪: {data[4].fat}g</li>
+          </ul>
+        </div>
+      </li>
+      {/* 品項2 e*/}
       {/* 品項3 s*/}
       <li className="ru-species-item ru-species-item3" id="">
         <div className="ru-species-img" id="ru-item3">
