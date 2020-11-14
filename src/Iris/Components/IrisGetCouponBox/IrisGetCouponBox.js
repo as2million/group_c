0e2161b943a6961a8b53305e69c32ded173cb9cf
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-11-15 02:31:17 +0800

    20201115 02:31am coupon add animation

--- src/Iris/Components/IrisGetCouponBox/IrisGetCouponBox.js
+++ src/Iris/Components/IrisGetCouponBox/IrisGetCouponBox.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './IrisGetCouponBox.scss'
 import { ReactComponent as Coupon30 } from './beastie_coupon30.svg'
+import { Animated } from 'react-animated-css'
 
 function IrisGetCouponBox(props) {
   const { setShowGetCouponBox } = props
@@ -16,6 +17,7 @@ function IrisGetCouponBox(props) {
         <div class="iris-coupon-img-wrapper">
           <Coupon30 />
         </div>
+
         <div class="iris-getCoupon-text1">新會員填寫資料</div>
         <div class="iris-getCoupon-text2">獲得30元怪獸幣</div>
       </div>
